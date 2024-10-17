6145feb13dc1a01d8c876ac596e372319df681e7
escribroid <sebascraton@gmail.com>
2024-10-17 16:48:11 -0300

    v0.8.6

--- js/mod-date.js
+++ js/mod-date.js
@@ -10,6 +10,8 @@ let canasta_month_select = document.getElementById("canasta_date_month");
 
 let cba;
 let cbt;
+let cba_a;
+let cbt_t;
 let canasta_compare_cba = document.querySelector(".canasta_compare_cba");
 let canasta_compare_cbt = document.querySelector(".canasta_compare_cbt");
 let canasta_compare_cbaja = document.querySelector(".canasta_compare_cbaja");
@@ -48,7 +50,7 @@ fetchDataFromAPI(
         const cbt_top_short = document.querySelector(".indices_short_cbt");
         cbt_top_short.innerHTML = `$${cbt}`;
 
-        //console.log("cba_cbt", data[2].fecha[0]);
+        //console.log("cba_cbt", data[2].cba);
 
         //selectedYear = canasta_year_select.value;
         //console.log("selectedYear2:", selectedYear);
@@ -149,8 +151,8 @@ function handleMonthChange(callbackMonth, callbackYear, callCba, callCbt) {
     canasta_month_select.addEventListener("change", function () {
         selectedMonth = canasta_month_select.value;
         selectedYear = canasta_year_select.value;
-        console.log("AÑO seleccionado en evento Month:", selectedYear);
-        console.log("Mes seleccionado en evento Month:", selectedMonth);
+        //console.log("AÑO seleccionado en evento Month:", selectedYear);
+        //console.log("Mes seleccionado en evento Month:", selectedMonth);
 
         if (selectedMonth !== "") {
             monthSelected = true;
@@ -169,7 +171,7 @@ function handleMonthChange(callbackMonth, callbackYear, callCba, callCbt) {
             (data) => {
                 // Data en TOP short cba cbt ++++++++++++++++++++++++
                 //cba = Math.round(data.data[data.data.length - 1][1] * 3.09);
-                console.log("data2", data[100]);
+                //console.log("data2", data[100]);
 
                 //cbt = Math.round(data.data[data.data.length - 1][2] * 3.09);
                 //console.log("cbt", cbt);
@@ -185,7 +187,7 @@ function handleMonthChange(callbackMonth, callbackYear, callCba, callCbt) {
 
                 for (let i = 0; i < data.length; i++) {
                     if (data[i].fecha == `${selectedYear}-${selectedMonth}-01`) {
-                        console.log("data[i].fecha", data[i].fecha);
+                        //console.log("data[i].fecha", data[i].fecha);
                         // console.log("date[i][0]", data.data[i][1]);
                         // console.log("cba_cbt[i][1]", data.data[i][2]);
                         canasta_compare_cba.innerHTML = `${data[i].cba}`;
@@ -290,10 +292,10 @@ function handleMonthChange(callbackMonth, callbackYear, callCba, callCbt) {
                 //selectedYear = canasta_year_select.value;
 
                 // Data en TOP short cba cbt ++++++++++++++++++++++++
-                cba = Math.round(data[data.length - 1][1] * 3.09);
+                //cba = Math.round(data[data.length - 1][1] * 3.09);
                 //console.log("cba 2", cba);
 
-                cbt = Math.round(data[data.length - 1][2] * 3.09);
+                //cbt = Math.round(data[data.length - 1][2] * 3.09);
                 //console.log("cbt 2", cbt);
 
                 //console.log("cba_cbt", data.data[2][0]);
@@ -327,6 +329,8 @@ function handleMonthChange(callbackMonth, callbackYear, callCba, callCbt) {
         if (callCbt) {
             callCbt(selectedYear, cbt);
         }
+        console.log("cba", cba);
+        console.log("cbt", cbt);
     });
 }
 
@@ -376,3 +380,64 @@ handleMonthChange(doSomethingWithSelectedMonth, doSomethingWithSelectedYear, doC
 // });
 
 //console.log("Mes seleccionado 4:", getSelectedMonth());
+
+let alquiler_past;
+let alquiler_past_value;
+let suma_CBA_Personas_past;
+let suma_CBT_Personas_past;
+let suma_CT_clase_baja_past;
+let suma_indigencia_past;
+let suma_pobreza_past;
+let suma_clase_baja_fragil_past;
+let suma_clase_baja_past;
+let suma_clase_media_fragil_past;
+let suma_clase_media_past;
+let suma_clase_media_alta_past;
+let suma_clase_alta_baja_past;
+
+function get_tabla_compare(suma_CBA_Personas_past, suma_CBT_Personas_past, alquiler_past_value, suma_con_alquiler) {
+    alquiler_past_value = parseInt(document.getElementById("alquiler_past").value);
+    //alquiler_out = document.getElementById("alquiler_out");
+    if (isNaN(alquiler_past_value)) {
+        alquiler_past_value = 0;
+    } else if (alquiler_past_value < 0) {
+        alquiler_past_value = alquiler_past_value * -1;
+    }
+
+    if (!suma_CBA_Personas_past || !suma_CBT_Personas_past) {
+        suma_CBA_Personas_past = 0;
+        suma_CBT_Personas_past = 0;
+    }
+
+    suma_CT_clase_baja_past = suma_CBT_Personas_past * 1.2;
+
+    suma_indigencia_past = alquiler_past_value + Math.round(suma_CBA_Personas_past);
+    suma_pobreza_past = alquiler_past_value + Math.round(suma_CBT_Personas_past);
+    suma_clase_baja_fragil_past = alquiler_past_value + Math.round(suma_CBT_Personas_past * 1.2);
+    suma_clase_baja_past = alquiler_past_value + Math.round(suma_CBT_Personas_past * 1.5);
+    suma_clase_media_fragil_past = alquiler_past_value + Math.round(suma_CBT_Personas_past * 2);
+    suma_clase_media_past = alquiler_past_value + Math.round(suma_CBT_Personas_past * 5);
+    suma_clase_media_alta_past = alquiler_past_value + Math.round(suma_CBT_Personas_past * 8);
+    suma_clase_alta_baja_past = alquiler_past_value + Math.round(suma_CBT_Personas_past * 12);
+
+    document.querySelector(".show_indigencia_min_past").textContent = 0;
+    document.querySelector(".show_indigencia_max_past").textContent = suma_indigencia_past;
+
+    document.querySelector(".show_pobreza_min_past").textContent = suma_indigencia_past;
+    document.querySelector(".show_pobreza_max_past").textContent = suma_pobreza_past;
+
+    document.querySelector(".show_clase_baja_min_past").textContent = suma_pobreza_past;
+    document.querySelector(".show_clase_baja_max_past").textContent = suma_clase_baja_past;
+
+    document.querySelector(".suma_clase_media_fragil_min_past").textContent = suma_clase_baja_past;
+    document.querySelector(".suma_clase_media_fragil_max_past").textContent = suma_clase_media_fragil_past;
+
+    document.querySelector(".suma_clase_media_min_past").textContent = suma_clase_media_fragil_past;
+    document.querySelector(".suma_clase_media_max_past").textContent = suma_clase_media_past;
+
+    document.querySelector(".suma_clase_media_alta_min_past").textContent = suma_clase_media_past;
+    document.querySelector(".suma_clase_media_alta_max_past").textContent = suma_clase_media_alta_past;
+
+    document.querySelector(".suma_clase_alta_min_past").textContent = suma_clase_media_alta_past;
+    document.querySelector(".suma_clase_alta_max_past").textContent = suma_clase_alta_baja_past;
+}
