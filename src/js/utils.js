f7b27539f569d6fbabe831565a15a0d254987d0e
escribroid <sebascraton@gmail.com>
2024-11-27 18:58:54 -0300

    v0.9.4.5

--- src/js/utils.js
+++ src/js/utils.js
@@ -9,3 +9,24 @@
 //         document.querySelector(".card-compare__header").style.borderRadius = "1rem";
 //     }
 // });
+
+// document.getElementById("card-canastas-link").addEventListener("click", function(){
+//   document.querySelector(".form-container").classList.add("form-recuadrar");
+// });
+
+// Escuchar el evento de clic en el documento para detectar el clic en el enlace del tooltip
+document.addEventListener("click", (event) => {
+    // Verificar si el clic ocurrió en el enlace del tooltip
+    if (event.target && event.target.id === "card-canastas-link") {
+        event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+
+        // Seleccionar el recuadro de destino
+        const recuadro = document.querySelector(".form-container");
+
+        // Agregar la clase para resaltar con borde rojo
+        recuadro.classList.add("form-recuadrar");
+
+        // Opcional: Desplazar la página al recuadro
+        recuadro.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+});
