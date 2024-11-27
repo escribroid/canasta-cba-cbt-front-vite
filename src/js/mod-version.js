fe01870ddc4f625e60fcc5ca89d16a5849baded1
escribroid <sebascraton@gmail.com>
2024-11-27 00:47:14 -0300

    v0.9.4.4

--- src/js/mod-version.js
+++ src/js/mod-version.js
@@ -1,1 +1,1 @@
-export const version = "beta | v0.9.4.3";
+export const version = "beta | v0.9.4.4";
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,11 +1,11 @@
-document.getElementById("SwitchCompareCba").addEventListener("change", function () {
-    if (this.checked) {
-        document.querySelector(".table-compare-body").style.display = "block";
-        let cardHeader = document.querySelector(".card-compare__header");
-        cardHeader.style.borderBottomLeftRadius = "0";
-        cardHeader.style.borderBottomRightRadius = "0";
-    } else {
-        document.querySelector(".table-compare-body").style.display = "none";
-        document.querySelector(".card-compare__header").style.borderRadius = "1rem";
-    }
-});
+// document.getElementById("SwitchCompareCba").addEventListener("change", function () {
+//     if (this.checked) {
+//         document.querySelector(".table-compare-body").style.display = "block";
+//         let cardHeader = document.querySelector(".card-compare__header");
+//         cardHeader.style.borderBottomLeftRadius = "0";
+//         cardHeader.style.borderBottomRightRadius = "0";
+//     } else {
+//         document.querySelector(".table-compare-body").style.display = "none";
+//         document.querySelector(".card-compare__header").style.borderRadius = "1rem";
+//     }
+// });
