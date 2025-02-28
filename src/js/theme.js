5224c0cc519072baf3b9ab2124a8553c19f02924
escribroid <sebascraton@gmail.com>
2025-02-28 10:56:36 -0300

    v0.9.6.9

---
+++ src/js/theme.js
@@ -0,0 +1,9 @@
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+);
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = "dark";
