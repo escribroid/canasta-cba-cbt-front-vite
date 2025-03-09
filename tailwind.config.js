b5549180deb8dee5071e18dff676890cd57c6896
escribroid <sebascraton@gmail.com>
2025-03-09 01:16:50 -0300

    v0.9.6.98

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+            }
+        },
     },
     plugins: [],
     corePlugins: {
