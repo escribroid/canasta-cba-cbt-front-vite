0810fcebff4650bb0bdaf8afc73436e9061add08
escribroid <sebascraton@gmail.com>
2025-03-24 02:44:02 -0300

    v0.9.6.99

--- src/js/mod-members.js
+++ src/js/mod-members.js
@@ -1,17 +1,18 @@
 //
-const table_members_model_1 = `<table class="w-full table-auto rounded-xl border border-neutral-600 bg-neutral-900								<thead class="">
+const table_members_model_1 = `<table class="min-w-full table-auto rounded-xl border border-neutral-600 bg-neutral-900">
+								<thead class="">
 									<tr class="border border-neutral-600">
-										<th class="border-r-4 border-blue-500 font-semibold px-2 py-2 text-left">Integrante</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Mujer</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Varon</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Mujer</th>
+										<th class="border-r-3 border-blue-500 font-semibold px-2 py-1.5 text-left">Integrante</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Mujer</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Varon</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Mujer</th>
 										
 									</tr>
 									<tr class="rounded-ee-full border border-neutral-600">
-										<th class="border-r-4 border-blue-500 font-semibold px-2 py-2 text-left">Edad</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">35</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">18</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">61</th>
+										<th class="border-r-3 border-blue-500 font-semibold px-2 py-1.5 text-left">Edad</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">35</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">18</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">61</th>
 									</tr>
 								</thead>
 							</table>`;
@@ -19,18 +20,18 @@ const table_members_model_1 = `<table class="w-full table-auto rounded-xl border
 const table_members_model_2 = `<table class="min-w-full table-auto rounded-xl border border-neutral-600 bg-neutral-900">
 							<thead class=" ">
 								<tr class=" py-5 border border-neutral-600">
-									<th class="border-r-3 border-blue-500 font-semibold px-2 py-2 text-left">Integrante</th>
-									<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Varon</th>
-									<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Mujer</th>
-									<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Niña</th>
-									<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Niño</th>
+									<th class="border-r-3 border-blue-500 font-semibold px-2 py-1.5 text-left">Integrante</th>
+									<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Varon</th>
+									<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Mujer</th>
+									<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Niña</th>
+									<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Niño</th>
 								</tr>
 								<tr class="rounded-ee-full border border-neutral-600">
-									<th class=" border-r-3 border-blue-500 font-semibold px-2 py-2 text-left">Edad</th>
-									<th class="font-normal border border-neutral-600 px-1 py-2 text-center">35</th>
-									<th class="font-normal border border-neutral-600 px-1 py-2 text-center">31</th>
-									<th class="font-normal border border-neutral-600 px-1 py-2 text-center">8</th>
-									<th class="font-normal border border-neutral-600 px-1 py-2 text-center">6</th>
+									<th class=" border-r-3 border-blue-500 font-semibold px-2 py-1.5 text-left">Edad</th>
+									<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">35</th>
+									<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">31</th>
+									<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">8</th>
+									<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">6</th>
 								</tr>
 							</thead>
 						</table>`;
@@ -38,20 +39,20 @@ const table_members_model_2 = `<table class="min-w-full table-auto rounded-xl bo
 const table_members_model_3 = `<table class="min-w-full table-auto rounded-xl border border-neutral-600 bg-neutral-900">
 								<thead class=" ">
 									<tr class="border border-neutral-600">
-										<th class="border-r-4 border-blue-500 text-white font-semibold p-1 text-left">Integrante</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Mujer</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Varon</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Niño</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Niño</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">Niño</th>
+										<th class="border-r-3 border-blue-500 text-white font-semibold p-1 text-left">Integrante</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Mujer</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Varon</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Niño</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Niño</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">Niño</th>
 									</tr>
 									<tr class="rounded-ee-full border border-neutral-600">
-										<th class="border-r-4 border-blue-500 text-white font-semibold p-1 text-left">Edad </th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">30</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">30</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">5</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">3</th>
-										<th class="font-normal border border-neutral-600 px-1 py-2 text-center">1</th>
+										<th class="border-r-3 border-blue-500 text-white font-semibold p-1 text-left">Edad </th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">30</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">30</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">5</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">3</th>
+										<th class="font-normal border border-neutral-600 px-1 py-1.5 text-center">1</th>
 									</tr>
 								</thead>
 							</table>`;
@@ -62,9 +63,9 @@ const selectMembers = document.querySelector('[data-select="select-models-famili
 
 const members_num = document.querySelector('[data-members-num="members-num"]');
 
-if (selectMembers.value === "1") {
-	members.innerHTML = table_members_model_1;
-	members_num.innerHTML = 3;
+if (selectMembers.value === "2") {
+	members.innerHTML = table_members_model_2;
+	members_num.innerHTML = 4;
 }
 
 document.querySelector('[data-select="select-models-families"]').addEventListener("change", function () {
